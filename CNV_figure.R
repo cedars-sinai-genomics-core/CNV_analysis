ef8495638163659963da99da1e931837a526b673
dxw5099 <dxw5099@gmail.com>
2020-06-12 16:13:30 -0700

    Update CNV_figure.R

--- CNV_figure.R
+++ CNV_figure.R
@@ -52,3 +52,34 @@ ggplot(mydata_1kb, aes(x=New_start, y=Samples)) + #geom_point() +
   #theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
   theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
 dev.off()
+
+
+
+# generate figure for shared and group-uniq CNVs 
+mydata <- read.csv("./CNV_visul.info_by_group.csv")
+mydata <- mydata[grep(paste(c("random", "chrUn", "chrM"),collapse="|"),mydata$chromosome, invert=TRUE),] 
+
+mydata$size <- mydata$end-mydata$start
+mydata$size_modified <- mydata$size
+mydata$size_modified[mydata$size_modified < 10000] = 10000
+
+mydata$End_modified <- mydata$start + mydata$size_modified
+mydata$New_start = mydata$start/1000000
+mydata$New_end = mydata$End_modified/1000000
+
+
+mydata$chromosome <- factor(mydata$chromosome, levels = c("chr1","chr2",  "chr3",  "chr4","chr5",  "chr6",  "chr7",  "chr8",  "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15" ,"chr16", "chr17", "chr18" ,"chr19","chrX",  "chrY"))
+
+pdf("./CNVs_per_chr_Mb_Manta_group.pdf",14,20)
+ggplot(mydata, aes(x=New_start, y=group)) + #geom_point() +
+  geom_segment(aes(xend=New_end, yend=group, colour=CNV_type)) +  #
+  geom_segment(aes(x=New_start, y=group, xend=New_end, yend=group, size=15, colour=CNV_type)) +  #, colour=CNV_type
+  #geom_hline(yintercept=2, linetype=2) +
+  facet_wrap(~ chromosome, scales = "free", ncol=2) +
+  xlab("Position (Mb)") +
+  ylab("Chromosome") +
+  scale_x_continuous(breaks = round(seq(0, max(mydata$New_end), by = 10),0)) + 
+  #scale_x_continuous(limits = c(0,200)) +
+  #scale_size_continuous(range = c(4:22000001, 22432001:22467000))+
+  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
+dev.off()
